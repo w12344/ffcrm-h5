07b0595f0c4435d7108f2ac5e2f6a6e8a5be80fe
W205349 <wu205349@163.com>
2026-04-08 19:51:41 +0800

    feat: use VITE_TOKEN to skip login

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'gh-pages' ? '/ffcrm-h5/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/ffcrm-h5/' : '/',
   plugins: [react()],
   server: {
     host: '0.0.0.0', // 允许外部访问

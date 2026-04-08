fe69e559315584ccd0b7a3aafb2c7b6202af24ee
W205349 <wu205349@163.com>
2026-04-08 11:57:07 +0800

    ✨feat: github action

--- .history/vite.config_20260408112755.ts
+++ .history/vite.config_20260408112755.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'gh-pages' ? '/ffcrm-h5/' : '/',
+  base: '/',
   plugins: [react()],
   server: {
     host: '0.0.0.0', // 允许外部访问
